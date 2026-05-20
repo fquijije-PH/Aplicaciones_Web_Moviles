5d1227eeedf8c036c11e47d0616f75783c2f05ef
fquijije-PH <franciscoquijijer@est.uepmrmp.edu.ec>
2026-05-20 09:51:03 -0500

    Add files via upload

---
+++ Unidad 1 /Variables/Variables.js
@@ -0,0 +1,11 @@
+//let una variable que cambia
+//const una variable constante
+//var una mezcla de ambos
+
+let nombre = "Francisco"
+console.log (nombre)
+//console.log sirve para imprimir por consola (mostrar en pantalla)
+const val = 1
+//Const no puede cambiar su valor a futuro
+console.log (val)
+val = 2
